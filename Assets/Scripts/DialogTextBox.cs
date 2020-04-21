a0e74ba389d776cdaeff602a818f875568ddb31f
Luz Arce <luzarce7@gmail.com>
2020-04-20 22:01:18 -0700

    Message NPC, animation

---
+++ Assets/Scripts/DialogTextBox.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogTextBox : MonoBehaviour
+{
+
+    [SerializeField]
+    Text dialog;
+    [SerializeField]
+    string message;
+
+    IEnumerator animateText;
+    [SerializeField, Range(0.01f, 4f)]
+    float animationsSpeed;
+
+    public void ClearText()
+    {
+        dialog.text = "";
+    }
+
+    public void ShowDialog()
+    {
+
+        animateText = Animate(animationsSpeed);
+        StartCoroutine(animateText);
+
+    }
+
+    /*public string TextDialog
+    {
+        set => dialog.text = value;
+    }*/
+
+    
+
+    public string Message { get => message; set => message = value; }
+
+    //IEnumerator 
+
+    IEnumerator Animate(float time)
+    {
+        
+        int i = 0;
+        while(i < Message.Length)
+        {
+            dialog.text += message[i];
+            i++;
+            yield return new WaitForSeconds(time); 
+        }
+
+        
+    }
+
+
+
+}
