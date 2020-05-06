70881cc0057dc372430aaf99eec051a528603cb8
Luz Arce <luzarce7@gmail.com>
2020-05-05 22:40:32 -0700

    Audio Background, battle

---
+++ Assets/Scripts/GameAudio.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class GameAudio
+{
+    
+    [SerializeField]
+    AudioClip background;
+
+    [SerializeField]
+    AudioClip battle;
+
+    AudioSource audioSource;
+
+    public AudioSource AudioSource { get => audioSource; set => audioSource = value; }
+
+    public void PlayAudio(AudioClip audioClip)
+    {
+
+        if(!ClipsEqual(audioClip))
+        {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+        
+    }
+
+    public void PlayBGMusic()
+    {
+        PlayAudio(background);
+    }
+
+    public void PlayBattleMusic()
+    {
+        PlayAudio(battle);
+    }
+
+    bool ClipsEqual(AudioClip clip)
+    {
+        return clip == audioSource.clip;
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,8 +11,12 @@ public class GameManager : MonoBehaviour
 
     bool isInCombat;
 
+    [SerializeField]
+    GameAudio gameAudio;
+
     public Player Player { get => player; }
     public bool IsInCombat { get => isInCombat; set => isInCombat = value; }
+    public GameAudio GameAudio { get => gameAudio; }
 
     void Awake()
     {
@@ -31,12 +35,19 @@ public class GameManager : MonoBehaviour
    
     }
 
+    private void Start()
+    {
+        gameAudio.AudioSource = GetComponent<AudioSource>();
+        gameAudio.PlayBGMusic();
+    }
+
     public void StartCombat()
     {
 
         //isInCombat = true;
         player.Anim.SetLayerWeight(1, 1);
         player.weaponVisible(true);
+        gameAudio.PlayBattleMusic();
 
     }
 
@@ -47,6 +58,7 @@ public class GameManager : MonoBehaviour
         player.Anim.SetLayerWeight(0, 1);
         player.Anim.SetLayerWeight(1, 0);
         player.weaponVisible(false);
+        gameAudio.PlayBGMusic();
 
     }
 
